191976159207ef44a207fd84d3ef1b63a589e807
Kshitijv24 <kshitijv24@gmail.com>
2024-06-27 23:46:28 +0530

    Added Data for the Category Model and Migrated it to the database

--- FirstProjectDotNetCoreMVC/FirstProject/Data/ApplicationDbContext.cs
+++ FirstProjectDotNetCoreMVC/FirstProject/Data/ApplicationDbContext.cs
@@ -11,5 +11,13 @@ namespace FirstProject.Data
         }
 
         public DbSet<CategoryModel> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryModel>().HasData(
+                new CategoryModel { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new CategoryModel { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new CategoryModel { Id = 3, Name = "Comedy", DisplayOrder = 3 });
+        }
     }
 }
--- FirstProjectDotNetCoreMVC/FirstProject/Migrations/20240627181422_SeedCategoryModelTable.Designer.cs
+++ FirstProjectDotNetCoreMVC/FirstProject/Migrations/20240627181422_SeedCategoryModelTable.Designer.cs
@@ -3,6 +3,7 @@ using FirstProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FirstProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240627181422_SeedCategoryModelTable")]
+    partial class SeedCategoryModelTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
